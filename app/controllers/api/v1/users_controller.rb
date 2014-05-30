7b0e3b46b5d3e45afd372b1b281a9b9c417a92ba
Jesus Marin <jesusangelm@gmail.com>
2014-05-30 15:13:08 -0430

    add actions show update

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -16,6 +16,21 @@ class Api::V1::UsersController < ApplicationController
     end
   end
 
+  def show
+    @user = User.find(params[:id])
+    render json: @user
+  end
+
+  def update
+    @user = User.find(params[:id])
+
+    if @user.update(user_params)
+      render json: @user, status: 200
+    else
+      render json: @user.errors, status: :unprocessable_entry
+    end
+  end
+
   def destroy
     @user = User.find(params[:id])
     @user.destroy
